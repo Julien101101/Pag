bc53ff9cb4802c3d307aefcdde0e61718abae72e
julien <juliensavary2001@gmail.com>
2024-03-21 12:42:57 -0700

    firebase_services.dart file addition

---
+++ lib/firebase_services.dart
@@ -0,0 +1,11 @@
+import 'package:firebase/firebase.dart' as fb;
+
+class FirebaseService {
+  void readFromDatabase() {
+    // Your read data function implementation here
+  }
+
+  void writeToDatabase() {
+    // Your write data function implementation here
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase/firebase.dart' as fb;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();          // Needed 4 
